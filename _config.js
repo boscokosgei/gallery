877663ef6d3cc0313f3f5137fe61f5b7aee7f84b
boscokosgei <kipkoech254kosgei@gmail.com>
2025-03-31 16:42:32 +0300

    updating test

--- _config.js
+++ _config.js
@@ -4,7 +4,7 @@ var config = {}
 config.mongoURI = {
     production: 'mongodb+srv://bosskoech:nHXcO9RFOhfUluX1@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
     development: 'mongodb+srv://bosskoech:nHXcO9RFOhfUluX1@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://bosskoech:nHXcO9RFOhfUluX1@gallery.yvznhac.mongodb.net//darkroom-test?retryWrites=true&w=majorit',
+    test: 'mongodb+srv://bosskoech:nHXcO9RFOhfUluX1@gallery.yvznhac.mongodb.net/darkroom-test?retryWrites=true&w=majorit',
 }
 module.exports = config;
 
